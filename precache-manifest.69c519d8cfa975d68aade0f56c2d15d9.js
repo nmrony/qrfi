f36dac8ab4d5ae3543feae2037d36b875bf5b36c
Nur Rony <pro.nmrony@gmail.com>
2019-01-18 19:24:16 +0600

    Updates

---
+++ precache-manifest.69c519d8cfa975d68aade0f56c2d15d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08df1b8b59a775b09b75",
+    "url": "/qrfi/static/js/runtime~main.08df1b8b.js"
+  },
+  {
+    "revision": "635b7b02bf7fb8bde8e1",
+    "url": "/qrfi/static/js/main.635b7b02.chunk.js"
+  },
+  {
+    "revision": "75f20d9337a8915b06bf",
+    "url": "/qrfi/static/js/1.75f20d93.chunk.js"
+  },
+  {
+    "revision": "635b7b02bf7fb8bde8e1",
+    "url": "/qrfi/static/css/main.1bd90f5b.chunk.css"
+  },
+  {
+    "revision": "7865dc355fd37c1ff5cc71913af370d0",
+    "url": "/qrfi/index.html"
+  }
+];
